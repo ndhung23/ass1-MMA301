fe01f0fef45e5866f1d250a68caeaf7021cf8879
ndhung23 <ndhung.work@gmail.com>
2026-09-20 20:11:11 +0700

    f

--- App.js
+++ App.js
@@ -141,7 +141,7 @@ const AllAppsScreen = () => {
 
         <TouchableOpacity style={styles.itemRow} activeOpacity={0.6}>
           <View style={styles.iconCircle}>
-            <RemoteIcon uri={`${BASE_URL}/01_app_features/`} width={42} height={42} />
+            <RemoteIcon uri={`${BASE_URL}/05_ui_error/ic_favorite_pear.png`} width={42} height={42} />
           </View>
           <View style={styles.itemContent}>
             <Text style={styles.itemTitle}>My Tasks</Text>
@@ -179,7 +179,7 @@ const AllAppsScreen = () => {
 
         <TouchableOpacity style={styles.itemRow} activeOpacity={0.6}>
           <View style={styles.iconCircle}>
-            <RemoteIcon uri={`${BASE_URL}/01_app_features/Survey_transparent.png`} width={42} height={42} />
+            <RemoteIcon uri={`${BASE_URL}/05_ui_error/ic_bg_check_in_online_course_survey.png`} width={42} height={42} />
           </View>
           <View style={styles.itemContent}>
             <Text style={styles.itemTitle}>Survey</Text>
@@ -203,7 +203,7 @@ const AllAppsScreen = () => {
 
         <TouchableOpacity style={styles.itemRow} activeOpacity={0.6}>
           <View style={styles.iconCircle}>
-            <RemoteIcon uri={`${BASE_URL}/01_app_features/`} width={42} height={42} />
+            <RemoteIcon uri={`${BASE_URL}/05_ui_error/payslip_trans.png`} width={42} height={42} />
           </View>
           <View style={styles.itemContent}>
             <Text style={styles.itemTitle}>Payslip</Text>
